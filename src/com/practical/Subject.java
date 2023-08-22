124872d751eab5ac92c15e74df7ca031e0db0af0
Raj Aryan Behera <rajaryanbehera@gmail.com>
2023-08-22 12:34:10 +0530

    subject_class

---
+++ src/com/practical/Subject.java
@@ -0,0 +1,29 @@
+package com.practical;
+
+public class Subject {
+	private int marks;
+	private String subName;
+	
+	public Subject(int marks, String subName) {
+		super();
+		this.marks = marks;
+		this.subName = subName;
+	}
+	public Subject() {
+		super();
+	}
+	public int getMarks() {
+		return marks;
+	}
+	public void setMarks(int marks) {
+		this.marks = marks;
+	}
+	public String getSubName() {
+		return subName;
+	}
+	public void setSubName(String subName) {
+		this.subName = subName;
+	}
+	
+	
+}
