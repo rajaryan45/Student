f551fc7df4322d84c89721959d4692b4d89447bd
shrishtipriya45 <sinhashrishtipriya@gmail.com>
2023-08-22 15:26:52 +0530

    feat_shri_student_subject_v1

--- src/com/practical/student_prac/Student.java
+++ src/com/practical/student_prac/Student.java
@@ -1,14 +1,16 @@
 package com.practical.student_prac;
 
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Student extends Human{
 
 	private int roll;
 	private int cls;
 	private char sec;
-	private int subCount;
-	private ArrayList<Subject> subjects;
+	private int subCount=0;
+	private ArrayList<Subject> subjects ;
+
 	
 	public Student(String name, int age, char gender) {
 		super(name, age, gender);
@@ -19,7 +21,13 @@ public class Student extends Human{
 		this.cls = cls;
 		this.sec = sec;
 	}
-
+	
+	public int getSubCount() {
+		return subCount;
+	}
+	public void setSubCount(int subCount) {
+		this.subCount = subCount;
+	}
 	public int getRoll() {
 		return roll;
 	}
@@ -40,8 +48,27 @@ public class Student extends Human{
 		this.sec = sec;
 	}
 	
-	
-	
+	public void setSubjects()
+	{
+		try (Scanner sc = new Scanner(System.in)) {
+			System.out.println("Enter the number of subjects:");
+			this.setSubCount(sc.nextInt());
+			sc.nextLine();
+			
+			for(int i=0;i<this.subCount;i++) {
+				Subject refSubject = new Subject();
+				System.out.println("\nEnter Subject " +(i+1) + "name :" );
+				refSubject.setSubName(sc.nextLine());
+				System.out.println("Enter Mark " +(i+1) + " : ");
+				refSubject.setMarks(sc.nextInt());
+				sc.nextLine();
+				subjects.add(refSubject);
+			}
+		}catch (Exception e) {
+			System.out.println(e.getMessage());
+		}
+			
+	}
 	public void showDetails() {
 		System.out.println("Name : " + this.getName() + "\nAge : " + this.getAge() + "\nGender : " +this.getGender() );
 		System.out.println("Roll : " + this.getRoll() + "\nClass : " + this.getCls() + "\nSection : " +this.getSec());
